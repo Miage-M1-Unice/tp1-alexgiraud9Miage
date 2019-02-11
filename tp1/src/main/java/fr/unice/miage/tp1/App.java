aab084782948f1ccb5cc34b6ed59338493c453e6
Alex Giraud <alex.giraud9@gmail.com>
2019-02-11 15:12:12 +0100

    Création du projet maven

---
+++ tp1/src/main/java/fr/unice/miage/tp1/App.java
@@ -0,0 +1,13 @@
+package fr.unice.miage.tp1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
