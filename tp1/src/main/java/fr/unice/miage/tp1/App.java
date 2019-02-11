0505d7618d327590258a62af66d2a7780788164f
Alex Giraud <alex.giraud9@gmail.com>
2019-02-11 16:21:32 +0100

    Ajout fonctionnalité filtres .java

--- tp1/src/main/java/fr/unice/miage/tp1/App.java
+++ tp1/src/main/java/fr/unice/miage/tp1/App.java
@@ -1,24 +1,43 @@
 package fr.unice.miage.tp1;
 
 import java.io.File;
+import java.io.FilenameFilter;
 
 public class App 
 {
     public static void main( String[] args ){
-    	findPathContent(".");
+    	App app = new App();
+    	app.findPathContent(".");
     }
     
     @SuppressWarnings("unused")
-	public static void findPathContent(String path) {
+	public void findPathContent(String path) {
     	File file = new File(path);
-        File[] listeFile = file.listFiles();     
+    	
+    	FilenameFilter filter = new FilenameFilter() {
+			public boolean accept(File dir, String name) {
+				boolean result = false;
+			
+				if(name.toLowerCase().endsWith(".java") || dir.isDirectory()) {
+					if(name.toLowerCase().endsWith(".java")){
+						System.out.println(dir.toString() + "\\"+ name);
+					}
+					result = true;
+				}
+				
+				return result;
+			}
+		};
+
+        File[] listeFile = file.listFiles(filter); 
+        
         
         if(file != null) {
         	for(File f : listeFile) {
             	if(f.isDirectory()) {
             		findPathContent(f.getPath());
             	}
-            	System.out.println(f.toString());
+            	//System.out.println(f.toString());
             }
         }else {
         	System.out.println("Le chemin est invalide !");
