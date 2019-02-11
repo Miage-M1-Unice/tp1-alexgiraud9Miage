1379eb8609a9b2d0265b652d6de7419537c20b42
Alex Giraud <alex.giraud9@gmail.com>
2019-02-11 15:21:41 +0100

    Liste du contenu du repertoire "."

--- tp1/src/main/java/fr/unice/miage/tp1/App.java
+++ tp1/src/main/java/fr/unice/miage/tp1/App.java
@@ -1,5 +1,7 @@
 package fr.unice.miage.tp1;
 
+import java.io.File;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,15 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	File file = new File(".");
+        String listeFile[] = file.list();      
+ 
+        if (listeFile != null) {         
+            for (int i = 0; i < listeFile.length; i++) {
+                System.out.println(listeFile[i]);
+            }
+        } else {
+            System.err.println("File invalide");
+        }
     }
 }
